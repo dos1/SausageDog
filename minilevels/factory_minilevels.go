3a8d3184e8fff4350118da343532d1e5e17ce3f7
Sebastian Krzyszkowiak <seba.dos1@gmail.com>
2017-02-18 03:02:35 +0100

    randomize minilevel generation

--- minilevels/factory_minilevels.go
+++ minilevels/factory_minilevels.go
@@ -30,3 +30,37 @@ embedded_components {
     w: 1.0
   }
 }
+embedded_components {
+  id: "level2factory"
+  type: "collectionfactory"
+  data: "prototype: \"/minilevels/level2.collection\"\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "level3factory"
+  type: "collectionfactory"
+  data: "prototype: \"/minilevels/level3.collection\"\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
