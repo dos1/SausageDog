c951aa963a5ee3164b05d4e614631d50d24d28d3
Wojciech Janas <janaswojciech@gmail.com>
2017-02-19 20:42:58 +0100

    polepszone minilevele

--- minilevels/factory_minilevels.go
+++ minilevels/factory_minilevels.go
@@ -115,3 +115,37 @@ embedded_components {
     w: 1.0
   }
 }
+embedded_components {
+  id: "level7factory"
+  type: "collectionfactory"
+  data: "prototype: \"/minilevels/level7.collection\"\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "level8factory"
+  type: "collectionfactory"
+  data: "prototype: \"/minilevels/level8.collection\"\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
