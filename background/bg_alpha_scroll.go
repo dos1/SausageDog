e3b01a3e0801bde4b2ea5ba7c9ea81ac5138cb77
Sebastian Krzyszkowiak <seba.dos1@gmail.com>
2017-02-26 03:01:53 +0100

    level pools and global game speed handling

--- background/bg_alpha_scroll.go
+++ background/bg_alpha_scroll.go
@@ -34,26 +34,6 @@ embedded_components {
   }
 }
 embedded_components {
-  id: "sprite1"
-  type: "sprite"
-  data: "tile_set: \"/background/background.atlas\"\n"
-  "default_animation: \"bg_alpha2\"\n"
-  "material: \"/builtins/materials/sprite.material\"\n"
-  "blend_mode: BLEND_MODE_ALPHA\n"
-  ""
-  position {
-    x: 4800.0
-    y: 540.0
-    z: -1.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
-  }
-}
-embedded_components {
   id: "sprite2"
   type: "sprite"
   data: "tile_set: \"/background/background.atlas\"\n"
@@ -63,26 +43,6 @@ embedded_components {
   ""
   position {
     x: 2880.0
-    y: 540.0
-    z: -1.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
-  }
-}
-embedded_components {
-  id: "sprite3"
-  type: "sprite"
-  data: "tile_set: \"/background/background.atlas\"\n"
-  "default_animation: \"bg_alpha2\"\n"
-  "material: \"/builtins/materials/sprite.material\"\n"
-  "blend_mode: BLEND_MODE_ALPHA\n"
-  ""
-  position {
-    x: 6720.0
     y: 540.0
     z: -1.0
   }
