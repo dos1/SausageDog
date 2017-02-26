b0f5ca6e5826b8765f1223b52fa1bded5eb21321
Wojciech Janas <janaswojciech@gmail.com>
2017-02-26 22:34:02 +0100

    więcej leveli, poprawione

--- minilevels/factory_minilevels.go
+++ minilevels/factory_minilevels.go
@@ -14,9 +14,9 @@ components {
   }
 }
 embedded_components {
-  id: "level1factory"
+  id: "level_a1factory"
   type: "collectionfactory"
-  data: "prototype: \"/minilevels/level1.collection\"\n"
+  data: "prototype: \"/minilevels/level_a1.collection\"\n"
   ""
   position {
     x: 0.0
@@ -31,9 +31,9 @@ embedded_components {
   }
 }
 embedded_components {
-  id: "level2factory"
+  id: "level_a2factory"
   type: "collectionfactory"
-  data: "prototype: \"/minilevels/level2.collection\"\n"
+  data: "prototype: \"/minilevels/level_a2.collection\"\n"
   ""
   position {
     x: 0.0
@@ -48,9 +48,9 @@ embedded_components {
   }
 }
 embedded_components {
-  id: "level3factory"
+  id: "level_a3factory"
   type: "collectionfactory"
-  data: "prototype: \"/minilevels/level3.collection\"\n"
+  data: "prototype: \"/minilevels/level_a3.collection\"\n"
   ""
   position {
     x: 0.0
@@ -65,9 +65,9 @@ embedded_components {
   }
 }
 embedded_components {
-  id: "level4factory"
+  id: "level_a4factory"
   type: "collectionfactory"
-  data: "prototype: \"/minilevels/level4.collection\"\n"
+  data: "prototype: \"/minilevels/level_a4.collection\"\n"
   ""
   position {
     x: 0.0
@@ -82,9 +82,9 @@ embedded_components {
   }
 }
 embedded_components {
-  id: "level5factory"
+  id: "level_a5factory"
   type: "collectionfactory"
-  data: "prototype: \"/minilevels/level5.collection\"\n"
+  data: "prototype: \"/minilevels/level_a5.collection\"\n"
   ""
   position {
     x: 0.0
@@ -99,9 +99,9 @@ embedded_components {
   }
 }
 embedded_components {
-  id: "level6factory"
+  id: "level_a6factory"
   type: "collectionfactory"
-  data: "prototype: \"/minilevels/level6.collection\"\n"
+  data: "prototype: \"/minilevels/level_a6.collection\"\n"
   ""
   position {
     x: 0.0
@@ -116,9 +116,9 @@ embedded_components {
   }
 }
 embedded_components {
-  id: "level7factory"
+  id: "level_b1factory"
   type: "collectionfactory"
-  data: "prototype: \"/minilevels/level7.collection\"\n"
+  data: "prototype: \"/minilevels/level_b1.collection\"\n"
   ""
   position {
     x: 0.0
@@ -133,9 +133,145 @@ embedded_components {
   }
 }
 embedded_components {
-  id: "level8factory"
+  id: "level_b2factory"
   type: "collectionfactory"
-  data: "prototype: \"/minilevels/level8.collection\"\n"
+  data: "prototype: \"/minilevels/level_b2.collection\"\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "level_b3factory"
+  type: "collectionfactory"
+  data: "prototype: \"/minilevels/level_b3.collection\"\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "level_b4factory"
+  type: "collectionfactory"
+  data: "prototype: \"/minilevels/level_b4.collection\"\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "level_b5factory"
+  type: "collectionfactory"
+  data: "prototype: \"/minilevels/level_b5.collection\"\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "level_b6factory"
+  type: "collectionfactory"
+  data: "prototype: \"/minilevels/level_b6.collection\"\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "level_c1factory"
+  type: "collectionfactory"
+  data: "prototype: \"/minilevels/level_c1.collection\"\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "level_c2factory"
+  type: "collectionfactory"
+  data: "prototype: \"/minilevels/level_c2.collection\"\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "level_c3factory"
+  type: "collectionfactory"
+  data: "prototype: \"/minilevels/level_c3.collection\"\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "level_s1factory"
+  type: "collectionfactory"
+  data: "prototype: \"/minilevels/level_s1.collection\"\n"
   ""
   position {
     x: 0.0
