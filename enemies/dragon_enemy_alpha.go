78b05fd678c68e05a972a77add475b42c4bc28d6
Andrei Isakau <isakov.andre@gmail.com>
2017-03-05 19:23:16 +0100

    ADDED: Dragon small and big animations MODIFIED: enemies alpha atlas, dragon enemy alpha and dragon enemy big alpha game objects

--- enemies/dragon_enemy_alpha.go
+++ enemies/dragon_enemy_alpha.go
@@ -64,7 +64,7 @@ embedded_components {
   id: "sprite"
   type: "sprite"
   data: "tile_set: \"/enemies/enemies_alpha.atlas\"\n"
-  "default_animation: \"dragon_alpha_3_little\"\n"
+  "default_animation: \"dragon_green_anim\"\n"
   "material: \"/builtins/materials/sprite.material\"\n"
   "blend_mode: BLEND_MODE_ALPHA\n"
   ""
