c1fbf896d59d4ddad7bb704be6a4db85068cc7f8
Wojciech Janas <janaswojciech@gmail.com>
2017-03-07 23:03:33 +0100

    MODIFIED: poczyszczone atlasy z niepotrzebnych obrazow

--- background/bg_alpha_scroll_hsmenu.go
+++ background/bg_alpha_scroll_hsmenu.go
@@ -16,8 +16,8 @@ components {
 embedded_components {
   id: "sprite"
   type: "sprite"
-  data: "tile_set: \"/background/background.atlas\"\n"
-  "default_animation: \"bg_beta\"\n"
+  data: "tile_set: \"/background/background_para.atlas\"\n"
+  "default_animation: \"bg_para_1\"\n"
   "material: \"/builtins/materials/sprite.material\"\n"
   "blend_mode: BLEND_MODE_ALPHA\n"
   ""
@@ -36,8 +36,8 @@ embedded_components {
 embedded_components {
   id: "sprite2"
   type: "sprite"
-  data: "tile_set: \"/background/background.atlas\"\n"
-  "default_animation: \"bg_beta\"\n"
+  data: "tile_set: \"/background/background_para.atlas\"\n"
+  "default_animation: \"bg_para_1\"\n"
   "material: \"/builtins/materials/sprite.material\"\n"
   "blend_mode: BLEND_MODE_ALPHA\n"
   ""
