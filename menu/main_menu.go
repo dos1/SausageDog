74b6c58f74e864ef3b7816de86ecfb084de7b6a7
Sebastian Krzyszkowiak <seba.dos1@gmail.com>
2017-03-10 22:45:34 +0100

    menu button

--- menu/main_menu.go
+++ menu/main_menu.go
@@ -1,7 +1,11 @@
 embedded_components {
   id: "hs"
   type: "sprite"
-  data: "tile_set: \"/menu/main_menu.atlas\"\ndefault_animation: \"button_highscore\"\nmaterial: \"/builtins/materials/sprite.material\"\nblend_mode: BLEND_MODE_ALPHA\n"
+  data: "tile_set: \"/menu/main_menu.atlas\"\n"
+  "default_animation: \"button_highscore\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
   position {
     x: 1419.505
     y: 263.44604
@@ -17,7 +21,11 @@ embedded_components {
 embedded_components {
   id: "music"
   type: "sprite"
-  data: "tile_set: \"/menu/main_menu.atlas\"\ndefault_animation: \"button_music\"\nmaterial: \"/builtins/materials/sprite.material\"\nblend_mode: BLEND_MODE_ALPHA\n"
+  data: "tile_set: \"/menu/main_menu.atlas\"\n"
+  "default_animation: \"button_music\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
   position {
     x: 457.64578
     y: 114.71041
@@ -33,7 +41,11 @@ embedded_components {
 embedded_components {
   id: "music_off"
   type: "sprite"
-  data: "tile_set: \"/menu/main_menu.atlas\"\ndefault_animation: \"music_off\"\nmaterial: \"/builtins/materials/sprite.material\"\nblend_mode: BLEND_MODE_ALPHA\n"
+  data: "tile_set: \"/menu/main_menu.atlas\"\n"
+  "default_animation: \"music_off\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
   position {
     x: 457.804
     y: 114.73333
@@ -49,7 +61,11 @@ embedded_components {
 embedded_components {
   id: "play"
   type: "sprite"
-  data: "tile_set: \"/menu/menu_rework.atlas\"\ndefault_animation: \"play\"\n"
+  data: "tile_set: \"/menu/menu_rework.atlas\"\n"
+  "default_animation: \"play\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
   position {
     x: 1422.9025
     y: 511.4038
@@ -65,7 +81,11 @@ embedded_components {
 embedded_components {
   id: "sound"
   type: "sprite"
-  data: "tile_set: \"/menu/main_menu.atlas\"\ndefault_animation: \"button_sound\"\nmaterial: \"/builtins/materials/sprite.material\"\nblend_mode: BLEND_MODE_ALPHA\n"
+  data: "tile_set: \"/menu/main_menu.atlas\"\n"
+  "default_animation: \"button_sound\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
   position {
     x: 770.92896
     y: 114.65449
@@ -81,7 +101,11 @@ embedded_components {
 embedded_components {
   id: "sounds_off"
   type: "sprite"
-  data: "tile_set: \"/menu/main_menu.atlas\"\ndefault_animation: \"sound_off\"\nmaterial: \"/builtins/materials/sprite.material\"\nblend_mode: BLEND_MODE_ALPHA\n"
+  data: "tile_set: \"/menu/main_menu.atlas\"\n"
+  "default_animation: \"sound_off\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
   position {
     x: 769.93066
     y: 114.73333
@@ -97,7 +121,11 @@ embedded_components {
 embedded_components {
   id: "title"
   type: "sprite"
-  data: "tile_set: \"/menu/title.atlas\"\ndefault_animation: \"title_sausage\"\nmaterial: \"/builtins/materials/sprite.material\"\nblend_mode: BLEND_MODE_ALPHA\n"
+  data: "tile_set: \"/menu/title.atlas\"\n"
+  "default_animation: \"title_sausage\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
   position {
     x: 924.338
     y: 860.9101
