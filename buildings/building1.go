0f06b8222cba5eefcde260c4cfbc5ea2ea003fd3
Wojciech Janas <janaswojciech@gmail.com>
2017-03-10 01:43:30 +0100

    MODIFIED - building z

--- buildings/building1.go
+++ buildings/building1.go
@@ -16,11 +16,15 @@ components {
 embedded_components {
   id: "sprite"
   type: "sprite"
-  data: "tile_set: \"/buildings/building.atlas\"\ndefault_animation: \"building_1\"\nmaterial: \"/builtins/materials/sprite.material\"\nblend_mode: BLEND_MODE_ALPHA\n"
+  data: "tile_set: \"/buildings/building.atlas\"\n"
+  "default_animation: \"building_1\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
   position {
     x: 0.0
     y: 0.0
-    z: -0.2
+    z: -0.3
   }
   rotation {
     x: 0.0
