d5263119840f23dd8977f7070e785fa06b015033
Sebastian Krzyszkowiak <seba.dos1@gmail.com>
2017-03-11 08:43:00 +0100

    Revert "MODIFIED: enemies sound deleted na dobre"
    This reverts commit 1ea85098bf234128734b3b2ae368b1709ef1e3a6.

--- enemies/samurai_enemy_alpha.go
+++ enemies/samurai_enemy_alpha.go
@@ -14,6 +14,26 @@ components {
   }
 }
 embedded_components {
+  id: "cat"
+  type: "sound"
+  data: "sound: \"/sounds/sound_cat.ogg\"\n"
+  "looping: 1\n"
+  "group: \"master\"\n"
+  "gain: 0.07943282\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
   id: "collisionobject"
   type: "collisionobject"
   data: "collision_shape: \"\"\ntype: COLLISION_OBJECT_TYPE_KINEMATIC\nmass: 0.0\nfriction: 0.1\nrestitution: 0.5\ngroup: \"default\"\nmask: \"default\"\nembedded_collision_shape {\n  shapes {\n    shape_type: TYPE_BOX\n    position {\n      x: -10.0\n      y: 0.0\n      z: 0.0\n    }\n    rotation {\n      x: 0.0\n      y: 0.0\n      z: -0.70710677\n      w: 0.70710677\n    }\n    index: 0\n    count: 3\n  }\n  data: 72.5\n  data: 20.0\n  data: 7.5\n}\nlinear_damping: 0.0\nangular_damping: 0.0\nlocked_rotation: false\n"
